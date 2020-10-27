fd27a4f72f564fc35e9302231f73b5aaa0174efd
Vladislav0101 <by.vlad01@mail.ru>
2020-10-27 04:21:39 +0300

    shelter

---
+++ README.md
@@ -0,0 +1,2 @@
+# vladislav0101-JS2020Q3
+Private repository for @vladislav0101
--- pages/pets/slider.js
+++ pages/pets/slider.js
@@ -1,9 +1,13 @@
 let pet = document.getElementsByClassName('elem');
-let name = document.getElementsByClassName('name');
-let img = document.getElementsByClassName('elem_Img');
+let name1 = document.getElementsByClassName('name1');
+let img1 = document.getElementsByClassName('elem_Img_1');
+let name2 = document.getElementsByClassName('name2');
+let img2 = document.getElementsByClassName('elem_Img_2');
+let name3 = document.getElementsByClassName('name3');
+let img3 = document.getElementsByClassName('elem_Img_3');
 let data = [{
         "name": "Jennifer",
-        "img": "assets/pets-jennifer.png",
+        "img": "../../assets/pets-jennifer.png",
         "type": "Dog",
         "breed": "Labrador",
         "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
@@ -20,7 +24,7 @@ let data = [{
     },
     {
         "name": "Sophia",
-        "img": "assets/pets-sophia.png",
+        "img": "../../assets/pets-sophia.png",
         "type": "Dog",
         "breed": "Shih tzu",
         "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
@@ -37,7 +41,7 @@ let data = [{
     },
     {
         "name": "Woody",
-        "img": "assets/pets-woody.png",
+        "img": "../../assets/pets-woody.png",
         "type": "Dog",
         "breed": "Golden Retriever",
         "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -55,7 +59,7 @@ let data = [{
     },
     {
         "name": "Scarlett",
-        "img": "assets/pets-scarlett.png",
+        "img": "../../assets/pets-scarlett.png",
         "type": "Dog",
         "breed": "Jack Russell Terrier",
         "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -72,7 +76,7 @@ let data = [{
     },
     {
         "name": "Katrine",
-        "img": "assets/pets-katrine.png",
+        "img": "../../assets/pets-katrine.png",
         "type": "Cat",
         "breed": "British Shorthair",
         "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -89,7 +93,7 @@ let data = [{
     },
     {
         "name": "Timmy",
-        "img": "assets/pets-timmy.png",
+        "img": "../../assets/pets-timmy.png",
         "type": "Cat",
         "breed": "British Shorthair",
         "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -107,7 +111,7 @@ let data = [{
     },
     {
         "name": "Freddie",
-        "img": "assets/pets-freddie.png",
+        "img": "../../assets/pets-freddie.png",
         "type": "Cat",
         "breed": "British Shorthair",
         "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -124,7 +128,7 @@ let data = [{
     },
     {
         "name": "Charly",
-        "img": "assets/pets-charly.png",
+        "img": "../../assets/pets-charly.png",
         "type": "Dog",
         "breed": "Jack Russell Terrier",
         "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
@@ -143,17 +147,71 @@ let data = [{
         ]
     }
 ]
-name.textContent = data[0].name;
-// name.addEventListener('');
 
-for (var i = 0; i < 8; i++) {
+for (let i = 0; i < 8; i++) {
     let j = (Math.random() * 7).toFixed();
-    // let eight = [0, 1, 2, 3, 4, 5, 6, 7];
-    // if (!eight[j]) {
-    //     i--;
-    // }
-    name[i].textContent = data[i].name;
-    // img[i].setAttribute('src', data[i].img);
-    // eight.splice(j, 1);
-    // console.log(j);
+    name1[i].textContent = data[i].name;
+    img1[i].setAttribute('src', `${data[i].img}`);
+    img1[i].style.backgroundImage = `url(${data[i].img})`;
 }
+for (let i = 0; i < 8; i++) {
+    let j = (Math.random() * 7).toFixed();
+    name2[i].textContent = data[i].name;
+    img2[i].setAttribute('src', `${data[i].img}`);
+    img2[i].style.backgroundImage = `url(${data[i].img})`;
+}
+for (let i = 0; i < 8; i++) {
+    let j = (Math.random() * 7).toFixed();
+    name3[i].textContent = data[i].name;
+    img3[i].setAttribute('src', `${data[i].img}`);
+    img3[i].style.backgroundImage = `url(${data[i].img})`;
+}
+
+let first = document.getElementById('first');
+let prev = document.getElementById('prev');
+let now = document.getElementById('now');
+let next = document.getElementById('next');
+let last = document.getElementById('last');
+let box = document.getElementById('slider_box');
+
+
+now.textContent = 1;
+let checkWidth = document.getElementById('checkWidth');
+first.onclick = function() {
+    box.style.transform = `translateX(0px)`;
+    now.textContent = 1;
+}
+prev.onclick = function() {
+    now.textContent = Number(now.textContent) - 1;
+    box.style.transform = `translateX(${+getComputedStyle(box).transform.split(',')[4] + checkWidth.clientWidth}px)`;
+    if (+now.innerHTML <= 0) {
+        box.style.transform = `translateX(${checkWidth.clientWidth*-2}px)`;
+        now.innerHTML = 3;
+    }
+    console.log(box.clientWidth);
+    console.log(getComputedStyle(box).transform.split(',')[4]);
+    console.log(getComputedStyle(box).transform);
+
+}
+next.onclick = function() {
+    now.textContent = Number(now.textContent) + 1;
+    box.style.transform = `translateX(${getComputedStyle(box).transform.split(',')[4] - +checkWidth.clientWidth}px)`;
+    if (+now.innerHTML >= 4) {
+        box.style.transform = `translateX(0px)`;
+        now.innerHTML = 1;
+    }
+    console.log(box.clientWidth);
+    console.log(getComputedStyle(box).transform);
+}
+last.onclick = function() {
+    now.textContent = 3;
+    box.style.transform = `translateX(${+checkWidth.clientWidth * -2}px)`;
+    console.log(box.clientWidth);
+    console.log(getComputedStyle(box).transform);
+}
+
+let moreBtn = document.querySelectorAll('friends--learn');
+moreBtn.onclick = () => {
+    alert('s');
+}
+console.log(moreBtn);
