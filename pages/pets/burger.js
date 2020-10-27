c73cb57025f27cea3bbacc58596f096fff6472bf
Vladislav0101 <by.vlad01@mail.ru>
2020-10-27 22:19:59 +0300

    shelter

--- pages/pets/burger.js
+++ pages/pets/burger.js
@@ -1,6 +1,6 @@
 let content = document.getElementById('burger_content');
 let burgerBtn = document.getElementById('burger_button');
 
-burgerBtn.onclick = () => {
-    content.style.display = 'block';
-}
+burgerBtn.addEventListener('click', () => {
+    content.classList.toggle('burger_translate');
+})
