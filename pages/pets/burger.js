1f4939c84b7aa378ec3c82244722aa43624102c1
Vladislav0101 <by.vlad01@mail.ru>
2020-10-27 17:15:43 +0300

    Shelter

---
+++ pages/pets/burger.js
@@ -0,0 +1,6 @@
+let content = document.getElementById('burger_content');
+let burgerBtn = document.getElementById('burger_button');
+
+burgerBtn.onclick = () => {
+    content.style.display = 'block';
+}
